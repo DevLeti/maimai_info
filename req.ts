c08b2797b1e3c07621dc12e8a1a0dce5585df7af
DevLeti <devleti0106@gmail.com>
2020-08-27 12:50:05 +0900

    modified req.ts

--- req.ts
+++ req.ts
@@ -2,6 +2,11 @@ import axios, { AxiosPromise } from 'axios';
 //import {UserResponse} from '@/types/user';
 
 const reqApi = axios.create({
-    baseURL: 'https://https://maimaidx-eng.com/maimai-mobile/record/musicLevel/',
+    baseURL: 'https://maimaidx-eng.com/maimai-mobile/record/musicLevel',
     timeout : 5000 // 5초
 })
+
+//get result print
+console.log(reqApi.get('/').then(function(response){
+    console.log(response);
+}));
